c243ccf47a260ad063eb1f8d20b66f41fec85b88
TheSanchari <sanchari678@gmail.com>
2020-12-12 17:22:36 +0530

    adding 404 path

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const unhandled = require('./routes/unhandled')
+
 
 const app = express()
 
 app.use(adminRoutes)
 app.use(shopRoutes)
+app.use(unhandled)
 
 app.use(bodyParser.urlencoded({
     extended : false
---
+++ routes/unhandled.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+router.use((req,res,next)=> {
+    res.status(404).send('<h1>Page not found</h1>')
+})
+
+module.exports = router
