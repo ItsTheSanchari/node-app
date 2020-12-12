e8c93e26f5b8a6a9688da7a2d0840f091381d885
TheSanchari <sanchari678@gmail.com>
2020-12-12 13:50:34 +0530

    basic server creation completed

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require('http');
-const fs = require('fs');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
+app.use((req,res,next)=> {
+    console.log('inside middleware')
+    next()
+})
 
-const server = http.createServer(routes.handler)
+const server = http.createServer(app)
 server.listen(3000)
