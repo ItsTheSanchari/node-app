7105097f5b5ad7424282c7270e9d6860249b8850
TheSanchari <sanchari678@gmail.com>
2020-12-23 12:39:56 +0530

    added path variable

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,6 @@ router.get('/',(req,res,next)=>{
     // res.send('<h1>Hello from the other world</h1>')
     // res.sendFile('../views/shop.html')
     // res.sendFile(path.join(__dirname,'../','views','shop.html'))
-    console.log('product list',admin.product)
     // res.sendFile(path.join(rootDir,'views','shop.html'))
     res.status(200).render('shop',{
         products : admin.product,
