adfd14adea08b67b6fd2eb20fc6e3fcece3153dc
ItsTheSanchari <sanchari678@gmail.com>
2021-02-25 22:16:56 +0530

    worked on user model

--- models/User.js
+++ models/User.js
@@ -1,19 +1,33 @@
-const Sequelize = require('sequelize')
-const sequelize = require('./../utils/database')
-
-const User = sequelize.define('users',{
-    id : {
-        type:Sequelize.INTEGER,
-        autoIncrement:true,
-        allowNull:false,
-        primaryKey:true
-    },
-    email : {
-        type:Sequelize.STRING,
-        allowNull:false,
-    },
-    name : {
-        type:Sequelize.STRING,
+const client = require('../utils/database')
+const mongoDb = require('mongodb')
+class User {
+    constructor(userName,email) {
+        this.userName = userName
+        this.email = email
     }
-})
-module.exports = User
+    save() {
+        return client.db('shop').collection('User').insertOne({
+            userName:this.userName,
+            email:this.email
+        }).then((userCreated) => {
+            console.log('userCreated',userCreated)
+        }).catch(error => {
+            console.log('error',error)
+        })
+    }
+
+    static findById(userId) {
+        return client.db('shop').collection("User").findOne({
+            _id : mongoDb.ObjectId(userId)
+        })
+        // .next()
+        // .then((User)=>{
+        //     console.log('found user',User)
+        // })
+        // .catch(error => {
+        //     console.log('error',error)
+        // })
+    }
+
+
+}
